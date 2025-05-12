850ea57a7ab53c8204aaa21a2f9c66c6c0d91cfb
Arijit Ghosh <144580430+ghoshariji@users.noreply.github.com>
2025-05-12 23:56:40 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,6 +4,7 @@ const API = axios.create({
   baseURL:`${import.meta.env.VITE_SERVER}`, // Change this to your actual backend URL
 });
 
+
 // Add token to headers
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
